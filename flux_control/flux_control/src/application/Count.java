0f951774a9e5cd7a3690e0a1c07fb1102cc3d3c1
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-09-04 20:44:41 -0300

    feat: flux control project finished

--- flux_control/flux_control/src/application/Count.java
+++ flux_control/flux_control/src/application/Count.java
@@ -1,10 +1,39 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import exceptions.InvalidParametersException;
+
 public class Count {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-
+		System.out.print("Enter the first number: ");
+		int num1 = sc.nextInt();
+		
+		System.out.print("Enter the second number: ");
+		int num2 = sc.nextInt();
+		
+		try {
+			count(num1, num2);
+		} catch(InvalidParametersException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();
+	}
+	
+	public static void count(int num1, int num2) throws InvalidParametersException{
+		if(num1 > num2) {
+			throw new InvalidParametersException();
+		}
+		
+		for(int i = 0; i < num2 - num1; i++) {
+			System.out.println("Printing number " + (i+1));
+		}
 	}
 
 }
--- flux_control/flux_control/src/exceptions/InvalidParametersException.java
+++ flux_control/flux_control/src/exceptions/InvalidParametersException.java
@@ -1,5 +1,10 @@
 package exceptions;
 
-public class InvalidParametersException {
-
+public class InvalidParametersException extends Exception{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	  public String getMessage(){
+	    return "The second number must be higher than the first one.";
+	  }
 }
